3103de0c6beee6a537054ff11bc55c8489eee5d7
Ramon181 <ramonantonio29970476@gmail.com>
2023-05-18 19:53:42 -0500

    Configuracio de laravel con la base de datos

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <div class=" flex justify-center max-w-[50%] m-auto p-4">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</body>
+</html>
